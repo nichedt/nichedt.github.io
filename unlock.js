ab7fef9b2d24baad35161e8dd5df02d82128c995
Nicholas Tan <nt325@cornell.edu>
2018-11-23 17:13:58 -0600

    minor cosmetic improvements

--- unlock.js
+++ unlock.js
@@ -6,9 +6,9 @@
   $(document).ready(function() {
     var option = $('[data-option]');
     
-    var keyHistory = ''
+    var keyHistory = '';
+    var historySize = 30;
     var passHash = '01ddac292398df1c7c7db718adde4f81cc5a7fad18ff2d88be6defada292f4d8a52b368a831a0dd749ce3e61e395d342cbeb994c4cffedf69906a76f96898d0a';
-    var messageLength = passHash.length;
     var match;
 
     $(document).keypress(function(e) {
@@ -26,14 +26,26 @@
             return
         }
 
-        // if(e.which == BACKSPACE) // BACKSPACE
-
         keyHistory += String.fromCharCode(e.which)
-        if (keyHistory.length > messageLength) {
+        if (keyHistory.length > historySize) {
             keyHistory = keyHistory.substr(1);
         }
 
     });
+
+    // $(document).keyDown(function(e) {
+    //     switch(e.keyCode) {
+    //         case 8:
+    //         case 46:
+    //         if (keyHistory != '') {
+    //             keyHistory = keyHistory.substr(0, keyHistory.length - 1);
+    //             console.log(keyHistory)
+    //         }
+    //         break;
+    //         default:
+    //         break;
+    //     }
+    // })
 
     var parts = location.pathname.split('/');
     $('a[href="' + parts[parts.length - 1] + '"]').addClass('active');
