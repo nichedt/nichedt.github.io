e65913b9e30af73b2c4a36858405d1854ef93267
Brandon Giraldo <brandongiraldo10@hotmail.com>
2020-10-25 13:00:42 -0700

    write own implmentation of read / write json

--- src/backend/routes/login/index.ts
+++ src/backend/routes/login/index.ts
@@ -1,7 +1,7 @@
 import { Router, Request, Response } from 'express';
 import bcrypt from 'bcrypt';
-import jsonfile from 'jsonfile'
 import path from 'path';
+import {loadJsonFile, writeToJsonFile} from "../../utils/jsonFileHandler";
 
 const usersFile = path.join(__dirname, '../../files/users.json');
 const router: Router = Router();
@@ -15,41 +15,44 @@ router.post('/', async (req: Request, res: Response) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
         const user: LoginCredentials = { username: req.body.username, password: hashedPassword }
-        jsonfile.readFile(usersFile, (err, obj) => {
-            if(obj.filter(item => item.username === user.username).length) {
-                res.status(500).send()
-            } else {
-                obj.push(user);
-                jsonfile.writeFile(usersFile, obj, (err) => res.status(err ? 500 : 201).send())
-            }
-        })
+        const data = await loadJsonFile(usersFile);
+        if(data.filter(item => item.username === user.username).length) {
+            res.status(500).send()
+        } else {
+            data.push(user);
+            const err = await writeToJsonFile(usersFile, data);
+            res.status(err ? 500 : 201).send();
+        }
     } catch {
         res.status(500).send()
     }
 });
 
-router.get('/', (req, res) => {
-    jsonfile.readFile(usersFile, (err, obj) => {
-        if(err) return res.status(500).send();
-        res.json(obj);
+router.get('/', async (req, res) => {
+    try {
+        const data = await loadJsonFile(usersFile);
+        res.json(data);
         res.status(200).send();
-    });
+    } catch(e) {
+        res.status(500).send();
+    }
 });
 
 router.post('/login', async (req, res) => {
-    // const user = users.find(user => user.username === req.body.username)
-    // if (user == null) {
-    //     return res.status(400).send('Cannot find user')
-    // }
-    // try {
-    //     if(await bcrypt.compare(req.body.password, user.password)) {
-    //         res.send('Success')
-    //     } else {
-    //         res.send('Not Allowed')
-    //     }
-    // } catch {
-    //     res.status(500).send()
-    // }
+    try {
+        const data = await loadJsonFile(usersFile);
+        const user = data.find(user => user.username === req.body.username)
+        if (user == null) {
+            return res.status(400).send('Cannot find user')
+        }
+        if(await bcrypt.compare(req.body.password, user.password)) {
+            res.send('Success')
+        } else {
+            res.send('Not Allowed')
+        }
+    } catch(e) {
+        res.status(500).send()
+    }
 });
 
 export const LoginRoute: Router = router;
